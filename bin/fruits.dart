6a0431acc71451839a7985a8077c90413c290a76
Rayhan <nm.rayhan.1650@gmail.com>
2024-03-16 00:00:55 +0600

    fruit details program completed

--- bin/fruits.dart
+++ bin/fruits.dart
@@ -1,21 +1,39 @@
 import 'package:fruits/fruits.dart' as fruits;
 
 void main() {
-  List<Map<String,String>> fruits=[
+  List<Map<String,dynamic>> fruits=[
     {
       'Name':'Apple',
       'Color':'Red',
-      'Price':'2.5',
+      'Price':2.5,
     },{
       'Name':'Banana',
       'Color':'Yellow',
-      'Price':'1.0',
+      'Price':1.0,
     },{
       'Name':'Grapes',
       'Color':'Purple',
-      'Price':'3.0',
+      'Price':3.0,
     }
   ];
+  displayFruitDetails(fruits);
+  applyPriceDiscount(fruits,10);
 }
 
+void displayFruitDetails(List<Map<String,dynamic>> fruits){
+  print('original Fruit details before discount:');
+  for(dynamic fruit in fruits){
+  print(fruit);
+  }
+}
 
+void applyPriceDiscount(List<Map<String,dynamic>> fruits, double discount){
+  print('Fruit details after applying $discount% discount:');
+  for(dynamic fruit in fruits){
+    fruit['Price']-=(fruit['Price']*discount)/100;
+  }
+
+  for(dynamic fruit in fruits){
+    print(fruit);
+  }
+}
