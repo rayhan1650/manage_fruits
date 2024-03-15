f8c16aee8fdfccd8a760f674c61c94f44be81556
Rayhan <nm.rayhan.1650@gmail.com>
2024-03-15 23:22:53 +0600

    fruit list created

--- bin/fruits.dart
+++ bin/fruits.dart
@@ -1,5 +1,21 @@
 import 'package:fruits/fruits.dart' as fruits;
 
-void main(List<String> arguments) {
-  print('Hello world: ${fruits.calculate()}!');
+void main() {
+  List<Map<String,String>> fruits=[
+    {
+      'Name':'Apple',
+      'Color':'Red',
+      'Price':'2.5',
+    },{
+      'Name':'Banana',
+      'Color':'Yellow',
+      'Price':'1.0',
+    },{
+      'Name':'Grapes',
+      'Color':'Purple',
+      'Price':'3.0',
+    }
+  ];
 }
+
+
